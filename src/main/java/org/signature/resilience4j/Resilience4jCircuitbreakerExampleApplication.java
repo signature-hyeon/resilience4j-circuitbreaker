8581d746cb70cf8e31d72ebe4237e4f85204cbbd
서명현 <azure394@gmail.com>
2025-07-10 12:21:17 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/org/signature/resilience4j/Resilience4jCircuitbreakerExampleApplication.java
@@ -0,0 +1,13 @@
+package org.signature.resilience4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Resilience4jCircuitbreakerExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Resilience4jCircuitbreakerExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/signature/resilience4j/Resilience4jCircuitbreakerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.signature.resilience4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resilience4jCircuitbreakerExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
